d264295ff4e62f6b206de2f9c8b84eef158868af
Sushant Neupane <nsushant09@gmail.com>
2021-12-21 19:02:26 +0545

    Another Calculator , but this one's better maybe.

--- Calculator2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Calculator2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
@@ -16,61 +17,133 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void showDisplay(View view,StringBuilder recieve_int){
+    String first = "0";
+    String second = "";
+    String operation = "+";
+    double total = 0;
+
+    @SuppressLint("SetTextI18n")
+    public void showDisplay(View view, String recieve_int){
         TextView displayObj = findViewById(R.id.Display);
-        StringBuilder string = new StringBuilder();
-        string.append(recieve_int);
-        double number = Double.parseDouble(string.toString());
-        double total = 0;
+        String recieved = recieve_int;
+        if(recieved.equals("+") || recieved.equals("-") || recieved.equals("*") || recieved.equals("/") || recieved.equals("=") || recieved.equals("C")){
+            operation = recieved;
+            first = total + "";
+            if(operation.equals("+") || operation.equals("-")){
+                second = "0";
+            }
+            else if (operation.equals("/") || operation.equals("*")){
+                second = "0";
+            }
+            else{
+                second = "";
+            }
+
+        }
+        else{
+            second+=recieved;
+        }
+
+        switch (operation){
+            case "+" :
+                total = Double.parseDouble(first) + Double.parseDouble(second);
+                displayObj.setText(total+"");
+                break;
+
+            case "-" :
+                total = Double.parseDouble(first) - Double.parseDouble(second);
+                displayObj.setText(total+"");
+                break;
+
+            case "*" :
+                if(second.equals("0")){
+                    total = Double.parseDouble(first) * 1;
+                }
+                else {
+                    total = Double.parseDouble(first) * Double.parseDouble(second);
+                }
+                displayObj.setText(total+"");
+                break;
+
+            case "/" :
+                if(second.equals("0")){
+                    total=Double.parseDouble(first) * 1;
+                }
+                else{
+                    total = Double.parseDouble(first) / Double.parseDouble(second);
+                }
+                displayObj.setText(total+"");
+                break;
+
+            case "C" :
+                total = 0 ;
+                first = "0";
+                second = "";
+                operation = "+";
+                displayObj.setText(total+"");
+                break;
+
+            case "=" :
+                displayObj.setText(total+"");
+
+        }
     }
 
     public void onClickOne(View view){
-        StringBuilder string_one = new StringBuilder("1");
-        showDisplay(view,string_one);
+        showDisplay(view,"1");
     }
 
     public void onClickTwo(View view){
-        StringBuilder string_two = new StringBuilder("2");
-        showDisplay(view,string_two);
+        showDisplay(view,"2");
     }
 
     public void onClickThree(View view){
-        StringBuilder string_three = new StringBuilder("3");
-        showDisplay(view,string_three);
+        showDisplay(view,"3");
     }
 
     public void onClickFour(View view){
-        StringBuilder string_four = new StringBuilder("4");
-        showDisplay(view,string_four);
+        showDisplay(view,"4");
     }
 
     public void onClickFive(View view){
-        StringBuilder string_five = new StringBuilder("1");
-        showDisplay(view,string_five);
+        showDisplay(view,"5");
     }
 
     public void onClickSix(View view){
-        StringBuilder string_six = new StringBuilder("6");
-        showDisplay(view,string_six);
+        showDisplay(view,"6");
     }
 
     public void onClickSeven(View view){
-        StringBuilder string_seven = new StringBuilder("7");
-        showDisplay(view,string_seven);
+        showDisplay(view,"7");
     }
     public void onClickEight(View view){
-        StringBuilder string_eight = new StringBuilder("8");
-        showDisplay(view,string_eight);
+        showDisplay(view,"8");
     }
 
     public void onClickNine(View view){
-        StringBuilder string_nine = new StringBuilder("9");
-        showDisplay(view,string_nine);
+        showDisplay(view,"9");
     }
 
     public void onClickZero(View view){
-        StringBuilder string_zero = new StringBuilder("0");
-        showDisplay(view,string_zero);
+        showDisplay(view,"0");
     }
 
+    public void onClickAdd(View view){
+        showDisplay(view,"+");
+    }
+    public void onClickSub(View view){
+        showDisplay(view, "-");
+    }
+    public void onClickMult(View view){
+        showDisplay(view, "*");
+    }
+    public void onClickDiv(View view){
+        showDisplay(view, "/");
+    }
+    public void onClickEqual(View view){
+        showDisplay(view, "=");
+    }
+    public void onClickCancle(View view){
+        showDisplay(view, "C");
+    }
 }
