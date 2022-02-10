1722bfad062a65fc68922018c8fd7d1340e15f4a
Sushant Neupane <nsushant09@gmail.com>
2022-02-10 17:07:54 +0545

    Building a school management system

---
+++ LearnImageSlider/app/src/main/java/com/example/learnimageslider/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.learnimageslider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    val handler : Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
