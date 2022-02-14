e7c7ed73567b396f7ac819dd7d2f4edc83f2945a
Sushant Neupane <nsushant09@gmail.com>
2022-02-14 18:30:25 +0545

    Learn to use a Scroll view

--- LearnScrollView/app/src/main/java/com/example/learnscroolview/MainActivity.kt
+++ LearnScrollView/app/src/main/java/com/example/learnscroolview/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.learnscroolview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Learn to use a scroll view
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
