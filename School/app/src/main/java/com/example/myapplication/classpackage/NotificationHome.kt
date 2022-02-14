4d4150550677876097d56160a0c87d76d603d79e
Sushant Neupane <nsushant09@gmail.com>
2022-02-14 18:28:00 +0545

    .

--- School/app/src/main/java/com/example/myapplication/classpackage/NotificationHome.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.classpackage
-
- class NotificationHome constructor(private val title : String, private val desc : String) {
-
-    fun getTitle() : String = title
-    fun getDesc() : String = desc
-}
