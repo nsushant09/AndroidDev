de457916cc2d22cb02ba07ce1c14fe110301c157
Sushant Neupane <nsushant09@gmail.com>
2022-02-17 12:21:05 +0545

    Added Lucky Number Feature

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,23 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val diceObject : Dice = Dice()
-
         btnRollDice.setOnClickListener{
-            diceObject.roll()
-            tvShowDiceNum.text = "The dice is rolled to : ${diceObject.getNum()}"
-            imgDice.setImageResource(Dice.imageList.get(diceObject.getNum()-1))
+            diceRolled(diceObject)
+        }
+    }
 
-            if(diceObject.getNum() == diceObject.getLuckyNum()){
-                layoutMainActivity.setBackgroundColor(resources.getColor(R.color.green))
-                tvWin.text = "Congratulations !! You Win"
-                tvChange.text = "The lucky number is randomly selected again"
-                diceObject.changeLucky()
-            }else{
-                layoutMainActivity.setBackgroundColor(resources.getColor(R.color.gold))
-                tvWin.text = ""
-                tvChange.text = ""
-            }
+    @SuppressLint("SetTextI18n")
+    fun diceRolled(diceObject : Dice){
+        diceObject.roll()
+        tvShowDiceNum.text = "The dice is rolled to : ${diceObject.getNum()}"
+        imgDice.setImageResource(Dice.imageList.get(diceObject.getNum()-1))
 
+        if(diceObject.getNum() == diceObject.getLuckyNum()){
+            layoutMainActivity.setBackgroundColor(resources.getColor(R.color.gold))
+            tvWin.text = "Congratulations !! You Win"
+            tvChange.text = "The lucky number is randomly selected again"
+            diceObject.changeLucky()
+        }else{
+            layoutMainActivity.setBackgroundColor(resources.getColor(R.color.silver))
+            tvWin.text = ""
+            tvChange.text = ""
         }
     }
 }
