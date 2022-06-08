6134b53dc8c611bfb20645e5523745a351112a49
Sushant Neupane <nsushant09@gmail.com>
2022-06-08 12:49:47 +0545

    Learn About Live Data and Android Application Architecture MVVM Model - View - ViewModel

---
+++ GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+
+    // The current word
+    val word = MutableLiveData<String>()
+    // The current score
+    val score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init{
+        resetList()
+        nextWord()
+        score.value = 0
+        Log.i("GameViewModel", "GameViewModel created")
+    }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+           // gameFinished()
+        } else {
+            word.value = wordList.removeAt(0)
+        }
+    }
+
+    fun onSkip() {
+        score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed")
+    }
+}
