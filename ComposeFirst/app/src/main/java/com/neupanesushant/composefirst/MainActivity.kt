a0cb9de4c857c48353e0a55c09c65674ec883293
Sushant Neupane <nsushant09@gmail.com>
2022-06-28 18:16:29 +0545

    Learnt Compose

--- ComposeFirst/app/src/main/java/com/neupanesushant/composefirst/MainActivity.kt
+++ ComposeFirst/app/src/main/java/com/neupanesushant/composefirst/MainActivity.kt
@@ -1,78 +1,81 @@
 package com.neupanesushant.composefirst
 
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.neupanesushant.composefirst.ui.theme.ComposeFirstTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeFirstTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally){
-                        Greeting(name = "Sushant")
-                        Text("This is another text", fontSize = 14.sp)
-                        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "This is a image", modifier = Modifier
-                            .width(40.dp)
-                            .height(80.dp))
-                    }
 
-
-                }
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(0.5f)
+                    .padding(top = 50.dp)
+            ) {
+                Text("Hello", modifier = Modifier
+                    .offset()
+                    .clickable {
+                        Toast
+                            .makeText(applicationContext, "Clicked Hello", Toast.LENGTH_SHORT)
+                            .show()
+                    })
+                Spacer(modifier = Modifier.height(30.dp))
+                Text("World")
+                ImageCard(painterResource(id = R.drawable.ic_launcher_background), "Icon Image", "This is the title")
             }
         }
 
-
     }
 
-    private fun intList(): List<Int> {
-        return listOf(1, 2, 3, 4, 5, 6, 7 ,8)
-    }
 
-    fun addToList() : List<String>{
-        return listOf("Sushant", "Neupane", "This", "Yogesh", "Bhatta", "Utsab", "Sapkota")
-    }
 }
-
-
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun ImageCard(
+    painter : Painter,
+    contentDescription : String,
+    title : String,
+    modifier : Modifier = Modifier
+){
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 8.dp
+    ){
+        Box(modifier = Modifier.height(200.dp), contentAlignment = Alignment.Center){
+            Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop, alignment = Alignment.Center)
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp), contentAlignment = Alignment.BottomCenter){
+                Text(title, style = TextStyle(color = Color.Black, fontSize = 16.sp))
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeFirstTheme {
-        Greeting("Android")
+        }
     }
 }
+
+
+
