07c55867da931e25b8ed29314d3b2dde0e843b69
Sushant Neupane <nsushant09@gmail.com>
2022-07-29 10:48:40 +0545

    changes: final commit

--- LearnCollapsingToolbar/app/src/main/java/com/neupanesushant/learncollapsingtoolbar/MainActivity.kt
+++ LearnCollapsingToolbar/app/src/main/java/com/neupanesushant/learncollapsingtoolbar/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.neupanesushant.learncollapsingtoolbar
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.neupanesushant.learncollapsingtoolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
