668d7ed3dcbab0bfd41370d0efe54eb7ecbe792b
Sushant Neupane <nsushant09@gmail.com>
2022-08-11 20:06:01 +0545

    new : Made expandable card using jetpack compose

--- ComposeFirst/app/src/main/java/com/neupanesushant/composefirst/MainActivity.kt
+++ ComposeFirst/app/src/main/java/com/neupanesushant/composefirst/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +20,8 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -31,8 +34,8 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier
                     .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight()
+                    .fillMaxWidth()
                     .padding(top = 50.dp)
             ) {
                 Text("Hello", modifier = Modifier
@@ -45,6 +48,9 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(30.dp))
                 Text("World")
                 ImageCard(painterResource(id = R.drawable.ic_launcher_background), "Icon Image", "This is the title")
+                Spacer(modifier = Modifier
+                    .height(30.dp))
+
             }
         }
 
@@ -76,6 +82,8 @@ fun ImageCard(
         }
     }
 }
+
+
 
 
 
