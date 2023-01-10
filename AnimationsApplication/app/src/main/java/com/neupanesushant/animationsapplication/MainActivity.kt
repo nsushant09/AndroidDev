1b7a169613a826d7f81363af71f37b6220a01b23
Sushant Neupane <nsushant09@gmail.com>
2023-01-10 09:36:55 +0545

    Progress bar help code

--- AnimationsApplication/app/src/main/java/com/neupanesushant/animationsapplication/MainActivity.kt
+++ AnimationsApplication/app/src/main/java/com/neupanesushant/animationsapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.neupanesushant.animationsapplication
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
@@ -12,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.layout.animation = AnimationUtils.loadAnimation(baseContext, R.anim.bounce)
+    }
+
+    @SuppressLint("Recycle")
+    private fun forLinearRadiusProgressBar(){
+        // code for progress bar in xml
+//        <ProgressBar
+//        android:id="@+id/detailsProgressBar"
+//        android:layout_width="match_parent"
+//        android:layout_height="4dp"
+//        style="@style/Widget.AppCompat.ProgressBar.Horizontal"
+//        android:indeterminate="false"
+//        android:progressDrawable="@drawable/nabil_nagarik_progress_bar_drawable"
+//        android:layout_marginBottom="4dp"
+//        />
+
+        //create object animator for animation i.e
+        var objectAnimator = ObjectAnimator()
+
+        //for animation
+//        objectAnimator = ObjectAnimator.ofInt(viewName, propertyName i.e "progress" , startValue, endValue)
+//
+//        if(objectAnimator.isRunning){
+//            objectAnimator.end()
+//        }
+//        objectAnimator.duration = 100;
+//        objectAnimator.start()
     }
 }
